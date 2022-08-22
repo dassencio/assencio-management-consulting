ba876ca24257ac1829c72f53b09dc921c342774c
Diego Assencio <diego@assencio.com>
2022-08-22 20:10:53 +0000

    Use router in hash mode

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 
 export const routes = [
@@ -25,7 +25,7 @@ export const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
