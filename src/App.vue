d4fbb8b34d05305868918e8f71b319e6caaa890b
Diego Assencio <diego@assencio.com>
2022-08-22 21:15:17 +0000

    Load local copy of "Open Sans" font

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,24 @@ import NavBar from "@/components/NavBar.vue";
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+@font-face {
+  font-family: "Open Sans Regular";
+  src: url("@/assets/opensans-regular.ttf") format("truetype");
+}
+@font-face {
+  font-family: "Open Sans Medium";
+  src: url("@/assets/opensans-medium.ttf") format("truetype");
+}
 
 html {
   background-color: white;
-  font-family: "Open Sans", sans-serif;
+  font-family: "Open Sans Regular", sans-serif;
   height: 100%;
+}
+h1,
+h2 {
+  font-family: "Open Sans Medium", sans-serif;
+  font-weight: normal;
 }
 a {
   text-decoration: none;
